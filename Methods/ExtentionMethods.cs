935e2d97905c42211d5c97bf92b8cd3112d41be2
JeyhunMst <109238733+JeyhunMst@users.noreply.github.com>
2022-08-18 02:58:35 +0400

    Initial commit

---
+++ Methods/ExtentionMethods.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace _18Aug_HomeWorkTask1.Methods
+{
+    static class ExtentionMethods
+    {
+        public static bool IsOdd(this int number) 
+        {
+            if (number % 2!=0)
+            {
+                return true;
+            }
+            return false;
+        
+        }
+        public static bool IsEven(this int number) 
+        {
+            if (number % 2 == 0)
+            {
+                return true;
+            }
+
+            return false;
+            //if (number.IsOdd() == false)
+            //{
+            //    return true;
+            //}
+            //else
+            //    return false;
+        }
+        public static bool IsContainsDigit(this string word) 
+        {
+            foreach (char item in word)
+            {
+                if (Char.IsDigit(item))
+                {
+                    return true;
+                }
+
+            }
+            return false;
+        }
+        public static string ToCapitalize(this string word) 
+        {
+            StringBuilder capitalize = new StringBuilder();
+            capitalize.Append(char.ToUpper(word[0]));
+            capitalize.Append(word.Substring(1).ToLower());
+            return capitalize.ToString();
+            
+        }
+        public static int[] GetValueIndexes(this string word,char symbol) 
+        {
+            int[] indexOf = new int[0];
+            int count = 0;
+            
+            for (int i = 0; i < word.Length; i++)
+            {
+                if (word[i] == symbol)
+                {
+                    count++;
+                    Array.Resize(ref indexOf, count);
+                    indexOf[count-1] = i;
+                    
+                }
+            }
+            return indexOf;
+            
+            
+        
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,32 @@
+﻿using _18Aug_HomeWorkTask1.Methods;
+using System;
+
+namespace _18Aug_HomeWorkTask1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int number = 342;
+            int NewNumb = 231;
+            string word = "ceyhun9712 mustafayev";
+            char symbol = 'e';
+            int[] array = word.GetValueIndexes(symbol);
+            foreach (int item in array)
+            {
+                Console.WriteLine(item);
+            }
+            Console.WriteLine(word.ToCapitalize());
+            Console.WriteLine(word.IsContainsDigit());
+            Console.WriteLine(number.IsEven());
+            Console.WriteLine(NewNumb.IsOdd());
+
+
+
+
+
+
+
+        }
+    }
+}
